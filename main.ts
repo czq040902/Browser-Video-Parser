2b3a7369ced0cb6e08d5d55cdec7cd0ab1ef1102
Yangdongping <16615812538@163.com>
2025-09-15 09:48:18 +0800

    调整依赖

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serveFile } from "jsr:@std/http/file-server";
+import { serveFile } from "@std/http/file-server";
 
 Deno.serve((req: Request) => {
   return serveFile(req, "./index.html");
